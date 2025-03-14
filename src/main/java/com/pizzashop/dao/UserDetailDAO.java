f4ae18cccb979496dabb07a593adca2928e2b64c
Shawn Osborne <shawnby222@gmail.com>
2025-03-14 14:55:44 -0500

    Removes unused classes

--- src/main/java/com/pizzashop/dao/UserDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.pizzashop.dao;
-
-import com.pizzashop.entities.UserDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDetailDAO extends JpaRepository<UserDetail, Integer> {
-}
